b1731d50190733e9212483e27ff8d39f350cc794
yoni2k <yoni.k81@gmail.com>
2019-09-11 15:06:18 +0300

    Adding ability to read from a DynamoDB table (and create it). So far resources created: DynamoDB table, API gateway, Lambda function, IAM policies

--- commands.sh
+++ commands.sh
@@ -3,3 +3,6 @@ aws s3 mb s3://yoni2k-first-sam1
 
 # package cloudformation from SAM template
 aws cloudformation package --s3-bucket yoni2k-first-sam1 --template-file template.yaml --output-template-file gen/template-generated.yaml
+
+# deploy cloudformation template
+aws cloudformation deploy --template-file gen\template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
--- src/app.py
+++ src/app.py
@@ -4,7 +4,14 @@ import os
 
 print('Loading function')
 
+# connect to a dynamo db in given region, 2 environment variables are given
+region_name = os.environ['REGION_NAME']
+dynamo = boto3.client('dynamodb', region_name=region_name)
+table_name = os.environ['TABLE_NAME']
+
+# Called in handler below
 def respond(err, res=None):
+    # returns a JSON format file with 200 and res received from lambda_handler below
     return {
         'statusCode': '400' if err else '200',
         'body': err.message if err else json.dumps(res),
@@ -13,7 +20,8 @@ def respond(err, res=None):
         },
     }
 
-
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
-    return respond(None, res='Hello world')
+    # Return scan of the dynamodb table
+    scan_result = dynamo.scan(TableName=table_name)
+    return respond(None, res=scan_result)
